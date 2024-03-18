e293370d2d21cb488adc7c395cbb7e6c4a7d3bf1
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 10:13:05 +0500

    Add files via upload

---
+++ 05_Question/index.js
@@ -0,0 +1,3 @@
+"use strict";
+// Print your favorite quote or any motivational quote.
+console.log('Imran Khan said:"We are not slave"');
---
+++ 05_Question/index.ts
@@ -0,0 +1,4 @@
+// Print your favorite quote or any motivational quote.
+
+
+console.log('Imran Khan said:"We are not slave"');
