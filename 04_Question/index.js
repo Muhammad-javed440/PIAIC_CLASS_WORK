074babf4e9c1cff7eab5a1f3bd43a4d07a39843c
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-18 10:11:03 +0500

    Add files via upload

---
+++ 04_Question/index.js
@@ -0,0 +1,4 @@
+"use strict";
+// Print `"Hello, "`, followed by your name (you can use any name).
+let name1 = "Muhammad Javed";
+console.log("Hello,", name1);
---
+++ 04_Question/index.ts
@@ -0,0 +1,4 @@
+// Print `"Hello, "`, followed by your name (you can use any name).
+
+let name1:string ="Muhammad Javed";
+console.log("Hello,",name1);
